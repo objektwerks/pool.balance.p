af68e29ecd6147baad4ac46198b39da1dd3085ac
objektwerks <objektwerks@runbox.com>
2023-05-15 11:11:46 -0400

    removed import

--- src/main/scala/pool/dashboard/TotalBrominePane.scala
+++ src/main/scala/pool/dashboard/TotalBrominePane.scala
@@ -1,7 +1,6 @@
 package pool.dashboard
 
 import pool.Context
-import pool.Measurement
 
 final class TotalBrominePane(context: Context) extends DashboardTitledPane(context):
   text = context.headerTotalBromine
