10206528e5e523c841aa7434314c58b2e88f83b7
objektwerks <objektwerks@runbox.com>
2023-05-15 11:10:24 -0400

    removed import

--- src/main/scala/pool/dashboard/CyanuricAcidPane.scala
+++ src/main/scala/pool/dashboard/CyanuricAcidPane.scala
@@ -1,7 +1,6 @@
 package pool.dashboard
 
 import pool.Context
-import pool.Measurement
 
 final class CyanuricAcidPane(context: Context) extends DashboardTitledPane(context):
   text = context.headerCyanuricAcid
