7cfc06339cd6766ec83ee2de63d1221350368cd3
objektwerks <objektwerks@runbox.com>
2023-05-15 11:05:02 -0400

    removed imports

--- src/main/scala/pool/App.scala
+++ src/main/scala/pool/App.scala
@@ -3,11 +3,7 @@ package pool
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
-import scalafx.Includes.*
 import scalafx.application.JFXApp3
-import scalafx.geometry.Insets
-import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
 
 object App extends JFXApp3 with LazyLogging:
   override def start(): Unit =
