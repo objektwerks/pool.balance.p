632c97b5157f60124643b0e564dcb623e7d13d39
objektwerks <objektwerks@runbox.com>
2025-04-08 07:03:59 -0400

    enhanced app

--- src/main/scala/pool/App.scala
+++ src/main/scala/pool/App.scala
@@ -6,17 +6,15 @@ import com.typesafe.scalalogging.LazyLogging
 import scalafx.application.JFXApp3
 
 object App extends JFXApp3 with LazyLogging:
-  override def start(): Unit =
-    val context = Context( ConfigFactory.load("app.conf") )
-    val view = View(context)
-    
+  logger.info("Starting app ...")
+
+  val context = Context( ConfigFactory.load("app.conf") )
+  val view = View(context)
+
+  override def start(): Unit =   
     stage = new JFXApp3.PrimaryStage:
       scene = view.scene
       title = context.windowTitle
       minWidth = context.windowWidth
       minHeight = context.windowHeight
       icons += context.logo
-
-    logger.info("App started.")
-
-  override def stopApp(): Unit = logger.info("App stopped.")
