11300ef35151a388c15f90f12966220b8c3d067c
objektwerks <objektwerks@runbox.com>
2025-04-08 07:05:37 -0400

    enhanced app

--- src/main/scala/pool/App.scala
+++ src/main/scala/pool/App.scala
@@ -3,6 +3,9 @@ package pool
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import java.awt.{Taskbar, Toolkit}
+import java.awt.Taskbar.Feature
+
 import scalafx.application.JFXApp3
 
 object App extends JFXApp3 with LazyLogging:
@@ -18,3 +21,10 @@ object App extends JFXApp3 with LazyLogging:
       minWidth = context.windowWidth
       minHeight = context.windowHeight
       icons += context.logo
+
+    if Taskbar.isTaskbarSupported() then
+      val taskbar = Taskbar.getTaskbar()
+      if taskbar.isSupported(Feature.ICON_IMAGE) then
+        val defaultToolkit = Toolkit.getDefaultToolkit()
+        val appIcon = defaultToolkit.getImage(getClass().getResource("/cipher.png"))
+        taskbar.setIconImage(appIcon)
