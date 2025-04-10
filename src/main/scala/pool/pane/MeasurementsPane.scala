c593c7ddb65091ec694ea2f29e4c58adb9b1be60
objektwerks <objektwerks@runbox.com>
2025-04-10 15:12:18 -0400

    edited if statement

--- src/main/scala/pool/pane/MeasurementsPane.scala
+++ src/main/scala/pool/pane/MeasurementsPane.scala
@@ -96,7 +96,7 @@ final class MeasurementsPane(context: Context, model: Model) extends VBox:
   VBox.setVgrow(tableView, Priority.Always)
 
   tableView.onMouseClicked = { event =>
-    if (event.getClickCount == 2 && tableView.selectionModel().getSelectedItem != null) update()
+    if (event.getClickCount == 2 && tableView.selectionModel().getSelectedItem != null) then update()
   }
 
   tableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
